3a131a35463146eb282349f3025c414191dea30a
woon rwee young <woonrweeyoung@gmail.com>
2024-04-27 00:53:39 +0800

    commit for pull, feautre to main

--- rebase.py
+++ rebase.py
@@ -1,12 +1,9 @@
-print("go hell")
+import random
 
-print("go hell")
+# Generate a list of 10 random numbers between 1 and 100
+random_numbers = [random.randint(1, 100) for _ in range(10)]
+print("Random numbers:", random_numbers)
 
-print("go hell")
-
-print("go hell")
-
-print("go hell")
-
-print("go hell")
-#x2
+# Sort the list of random numbers
+sorted_numbers = sorted(random_numbers)
+print("Sorted numbers:", sorted_numbers)
