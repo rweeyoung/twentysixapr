213cb1bd60e66e77aa8b3173dcad81d9b8e0bc1e
woon rwee young <woonrweeyoung@gmail.com>
2024-04-27 01:34:04 +0800

    into main-remove fku

--- rebase.py
+++ rebase.py
@@ -7,10 +7,3 @@ print("Random numbers:", random_numbers)
 # Sort the list of random numbers
 sorted_numbers = sorted(random_numbers)
 print("Sorted numbers:", sorted_numbers)
-
-print("fku")
-print("fku")
-print("fku")
-print("fku")
-print("fku")
-print("fku")
